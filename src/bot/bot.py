c6cf309ff79a5db7d496ca61f8b58595300b4b55
RadikalRandy <randylwallace1@yahoo.com>
2025-07-18 09:53:18 -0500

    Update bot.py

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -9,7 +9,7 @@ from telethon import TelegramClient
 from telethon.errors.rpcerrorlist import UnauthorizedError
 
 from src.handlers import (
-    bard_chat_handler,
+    bard_chat_handler,whi
     bash_handler,
     bing_chat_handler,
     clear_handler,
@@ -32,57 +32,44 @@ def load_keys() -> Tuple[str, int, str]:
     api_id = os.getenv("API_ID")
     api_hash = os.getenv("API_HASH")
     bot_token = os.getenv("BOTTOKEN")
-    return api_id, api_hash, bot_token
+    return int(api_id), api_hash, bot_token
 
 
 async def bot() -> None:
-    while True:
-        api_id, api_hash, bot_token = load_keys()
-        try:
-            client = await TelegramClient(None, api_id, api_hash).start(
-                bot_token=bot_token
-            )
-            logging.info("Successfully initiate bot")
-        except UnauthorizedError:
-            logging.error(
-                "Unauthorized access. Please check your Telethon API ID, API hash"
-            )
-            raise UnauthorizedError
-        except Exception as e:
-            logging.error(f"Error occurred: {e}")
-            raise e
+    api_id, api_hash, bot_token = load_keys()
+    
+    try:
+        client = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
+        logging.info("Successfully initiated bot")
+    except UnauthorizedError:
+        logging.error("Unauthorized access. Please check your Telethon API ID, API hash")
+        raise UnauthorizedError
+    except Exception as e:
+        logging.error(f"Error occurred: {e}")
+        raise e
 
-        client.add_event_handler(security_check)
+    client.add_event_handler(security_check)
+    client.add_event_handler(search_handler)
+    logging.debug("Search handler added")
+    client.add_event_handler(bash_handler)
+    logging.debug("Bash handler added")
+    client.add_event_handler(clear_handler)
+    logging.debug("Clear handler added")
+    client.add_event_handler(switch_model_handler)
+    logging.debug("Switch model handler added")
+    client.add_event_handler(bard_chat_handler)
+    logging.debug("Bard chat handler added")
+    client.add_event_handler(bing_chat_handler)
+    logging.debug("Bing chat handler added")
+    client.add_event_handler(gemini_chat_handler)
+    logging.debug("Gemini chat handler added")
+    client.add_event_handler(senpai_chat_handler)
+    logging.debug("Senpai chat handler added")
+    client.add_event_handler(group_chat_handler)
+    logging.debug("Group chat handler added")
+    client.add_event_handler(user_chat_handler)
+    logging.debug("User chat handler added")
 
-        # Search feature
-        client.add_event_handler(search_handler)
-        logging.debug("Search handler added")
-
-        # Terminal bash feature
-        client.add_event_handler(bash_handler)
-        logging.debug("Bash handler added")
-
-        # Clear chat history feature
-        client.add_event_handler(clear_handler)
-        logging.debug("Clear handler added")
-
-        # Switch gpt model
-        client.add_event_handler(switch_model_handler)
-        logging.debug("Switch model handler added")
-
-        # User and group chat
-        client.add_event_handler(bard_chat_handler)
-        logging.debug("Bard chat handler added")
-        client.add_event_handler(bing_chat_handler)
-        logging.debug("Bing chat handler added")
-        client.add_event_handler(gemini_chat_handler)
-        logging.debug("Gemini chat handler added")
-        client.add_event_handler(senpai_chat_handler)
-        logging.debug("Senpai chat handler added")
-        client.add_event_handler(group_chat_handler)
-        logging.debug("Group chat handler added")
-        client.add_event_handler(user_chat_handler)
-        logging.debug("User chat handler added")
-
-        print("Bot is running")
-        await client.run_until_disconnected()
+    print("Bot is running")
+    await client.run_until_disconnected()
+    
